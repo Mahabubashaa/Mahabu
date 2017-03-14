ff2b403ce22facda8bd73d4d78bf42d794f55705
Mahabubashaa <Mahabubashaa@gmail.com>
2017-03-14 15:31:07 +0530

    Update SampleTC_for_Git.java

--- src/sample/SampleTC_for_Git.java
+++ src/sample/SampleTC_for_Git.java
@@ -3,7 +3,7 @@ package sample;
 public class SampleTC_for_Git {
 
 	public static void main(String[] args) {
-		System.out.println("Practise session for Git Hub");
+		System.out.println("Practise session for Git Hub00000");
 		
 	}
 
