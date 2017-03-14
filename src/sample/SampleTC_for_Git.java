12e84f975f08d8cba4c0edf8dd0d4709d7823a14
mahabubashaa@gmail.com <mahabubashaa@gmail.com>
2017-03-14 15:30:39 +0530

    hnuh

--- src/sample/SampleTC_for_Git.java
+++ src/sample/SampleTC_for_Git.java
@@ -3,7 +3,7 @@ package sample;
 public class SampleTC_for_Git {
 
 	public static void main(String[] args) {
-		System.out.println("Practise session for Git Hub123");
+		System.out.println("Practise session for Git Hub");
 		
 	}
 
